5c05fc948158b859c35e5d63443465a986fd292c
Frode Holmer <fholmer@gmail.com>
2019-07-29 15:02:52 +0200

    BaseAsyncController: allow shared event loop

--- netdef/Controllers/BaseAsyncController.py
+++ netdef/Controllers/BaseAsyncController.py
@@ -11,14 +11,17 @@ class BaseAsyncController(BaseController.BaseController):
     def __init__(self, name, shared):
         super().__init__(name, shared)
         self.init_asyncio()
-
+    
     def init_asyncio(self):
         # egen eventloop bare for denne kontrolleren
-        self.loop = asyncio.new_event_loop()
+        self.loop = self.get_event_loop()
 
         # dette signalet mottas når program avsluttes
         self.interrupt_loop = asyncio.locks.Event(loop=self.loop)
         self.enter_running_state = asyncio.locks.Event(loop=self.loop)
+
+    def get_event_loop(self):
+        return asyncio.new_event_loop()
 
     def loop_incoming_until_interrupt(self):
         # denne funksjonen kjører som en vanlig blokkerende tråd i asyncio
